e817bb4248b6cc5620c986257b010ac2443fba27
Mael Fioroni <mafioron@f2r2s12.clusters.42paris.fr>
2025-12-03 21:34:12 +0100

    need to do wordpress

--- srcs/requirements/mariadb/tools/init.sh
+++ srcs/requirements/mariadb/tools/init.sh
@@ -2,24 +2,27 @@
 
 set -e
 
-DB_PASSWORD=$( cat run/secrets/db_password.txt )
-ROOT_PASSWORD=$( cat run/secrets/db_root_password.txt )
+DB_PASSWORD=$( cat /run/secrets/db_password )
+ROOT_PASSWORD=$( cat /run/secrets/db_root_password )
 
-if [ ! -d /var/lib/mysql/mysql]; then
+if [ ! -d /var/lib/mysql/mysql ]; then
 	echo "Initalizing MariaDB";
 
-	mysql_install_db --user=mysql --datadir=data/lib/mysql;
+	mysql_install_db --user=mysql --datadir=/var/lib/mysql;
 	
 	mysqld --user=mysql --bootstrap << EOF
-	FLUSH PRIVILEGES
+	FLUSH PRIVILEGES;
+	CREATE DATABASE IF NOT EXISTS `${MYSQL_DATABASE}`;
 	CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}';
-	GRANT ALL PRIVILEGES ON '${MYSQL_DATABASE}'.*@'%' TO '${MYSQL_USER}'@'%';
+	GRANT ALL PRIVILEGES ON `${MYSQL_DATABASE}`.* TO '${MYSQL_USER}'@'%';
 	ALTER USER 'root'@'localhost' IDENTIFIED BY '${ROOT_PASSWORD}';
-	FLUSH PRIVILEGES
+	DELETE FROM mysql.user WHERE User='';
+	DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
+	FLUSH PRIVILEGES;
 EOF
 
 fi
 
 
 
-exec mysqld --user=mysql
+exec mysqld --user=mysql --console
